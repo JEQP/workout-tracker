51471c2b869ae92b3aad054f28bd67a5a6a42294
James Pearce <jamesquintanapearce@gmail.com>
2020-05-10 11:48:26 +1000

    CSS adjustments for clarity

--- public/api.js
+++ public/api.js
@@ -10,6 +10,7 @@ const API = {
 
     return json[json.length - 1];
   },
+  
   async addExercise(data) {
     
     const id = location.search.split("=")[1];
@@ -24,6 +25,7 @@ const API = {
 
     return json;
   },
+  
   async createWorkout(data = {}) {
     const res = await fetch("/api/workouts", {
       method: "POST",
